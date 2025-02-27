3028d9a7d3d9594a4aa15124f0e50a175573fb38
Ali Gurur Yaman <aligururyaman@gmail.com>
2025-02-27 11:51:52 +0300

    cache

--- next.config.js
+++ next.config.js
@@ -1,46 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "1000logos.net",
-      },
-      {
-        protocol: "https",
-        hostname: "upload.wikimedia.org",
-      },
-      {
-        protocol: "https",
-        hostname: "logos-world.net",
-      },
-      // Diğer logo sitelerini de buraya ekleyebilirsiniz
-    ],
-    domains: [],
-    formats: ["image/avif", "image/webp"],
+    formats: ["image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
-    minimumCacheTTL: 31536000, // 1 yıl
   },
-  experimental: {
-    optimizeCss: true,
-    optimizeImages: true,
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
-  },
-  webpack: (config, { dev, isServer }) => {
-    // Üretim ortamında bundle analizi
-    if (!dev && !isServer) {
-      Object.assign(config.optimization.splitChunks.cacheGroups, {
-        commons: {
-          name: "commons",
-          chunks: "all",
-          minChunks: 2,
-          priority: 10,
-        },
-      });
-    }
+  webpack: (config) => {
     return config;
   },
 };
